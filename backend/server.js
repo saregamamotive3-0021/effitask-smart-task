7f8ee0782c0a3e0e6d634fba8fafbb6477f996f5
saregamamotive3-0021 <saregamamotive3@gmail.com>
2026-07-01 20:58:24 +0530

    Backend added!!!

---
+++ backend/server.js
@@ -0,0 +1,321 @@
+const express = require("express");
+const mysql = require("mysql2");
+const cors = require("cors");
+const bcrypt= require("bcrypt");
+const fs= require("fs");
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const connection = mysql.createConnection({
+    host: "mysql-363a54f1-effitask.l.aivencloud.com",
+    user: "avnadmin",
+    password: "AVNS_899PbVSoMb--Or4bQY-",
+    database: "defaultdb",
+    port: 12679,
+    ssl: {
+        ca: fs.readFileSync("./ca.pem"),
+    },
+});
+
+
+connection.connect((err) => {
+    if (err) {
+        console.error(" Database connection failed:", err.message);
+        process.exit(1); // Exit with failure
+    }
+    console.log("Connected to MySQL database successfully!");
+});
+
+app.post("/addTask", (req, res) => {
+    console.log("BODY:", req.body);
+
+    const { text, priority, startDate, endDate, userId } = req.body;
+
+    const sql = `
+        INSERT INTO TASKS
+        (task_name, priority, start_date, end_date, user_id)
+        VALUES (?, ?, ?, ?, ?)
+    `;
+
+    connection.query(
+        sql,
+        [text, priority, startDate, endDate, userId],
+        (err, result) => {
+            if (err) {
+                console.error(err);
+                return res.status(500).json({
+                    message: "Failed to save task",
+                });
+            }
+
+            res.status(200).json({
+                message: "Task saved successfully",
+                id: result.insertId,
+            });
+        },
+    );
+});
+
+app.get("/tasks/:userId", (req, res) => {
+    const { userId } = req.params;
+
+    const sql = `
+        SELECT *
+        FROM TASKS
+        WHERE user_id = ?
+        AND is_deleted = FALSE
+    `;
+
+    connection.query(sql, [userId], (err, result) => {
+        if (err) {
+            return res.status(500).json({
+                message: "Error fetching tasks",
+            });
+        }
+
+        res.json(result);
+    });
+});
+
+app.get("/tasks", (req, res) => {
+    const sql = `
+        SELECT *
+        FROM TASKS
+        WHERE is_deleted = FALSE
+    `;
+
+    // connection.query(sql, (err, results) => {
+    //     if (err) {
+    //         console.error(err);
+    //         return res.status(500).json({
+    //             message: "Error fetching tasks"
+    //         });
+    //     }
+
+    //     res.json(results);
+    // });
+
+    connection.query(sql, (err, result) => {
+        if (err) {
+            console.log(err);
+            return res.status(500).json({
+                message: "Error fetching tasks",
+            });
+        }
+
+        res.json(result);
+    });
+});
+
+app.put("/delete/:id", (req, res) => {
+    const { id } = req.params;
+
+    const sql = `
+        UPDATE TASKS
+        SET is_deleted = TRUE
+        WHERE id = ?
+    `;
+
+    connection.query(sql, [id], (err, result) => {
+        if (err) {
+            console.log(err);
+            return res.status(500).json({
+                message: "Error deleting task",
+            });
+        }
+
+        res.json({
+            message: "Task deleted successfully",
+        });
+    });
+});
+
+//Login
+app.post("/signup", async (req, res) => {
+    const { name, email, password } = req.body;
+
+    // Email validation
+    const emailRegex =
+        /^[a-zA-Z0-9._%+-]+@(gmail\.com|outlook\.com|rediffmail\.com)$/i;
+
+    if (!emailRegex.test(email)) {
+        return res.status(400).json({
+            message: "Enter appropriate mail"
+        });
+    }
+
+    // Password validation (NEW)
+    const passwordRegex =
+        /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/;
+
+    if (!passwordRegex.test(password)) {
+        return res.status(400).json({
+            message:
+                "Password must be at least 8 characters long and include letters, numbers, and a special character"
+        });
+    }
+
+    try {
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const sql =
+            "INSERT INTO LOGIN (name, email, password) VALUES (?, ?, ?)";
+
+        connection.query(sql, [name, email, hashedPassword], (err) => {
+            if (err) {
+                return res.status(500).json({
+                    message: err.message
+                });
+            }
+
+            res.status(201).json({
+                message: "User registered successfully"
+            });
+        });
+    } catch (err) {
+        res.status(500).json({
+            message: "Hashing error"
+        });
+    }
+});
+
+app.post("/login", (req, res) => {
+    const { email, password } = req.body;
+
+    const sql = "SELECT * FROM LOGIN WHERE email = ?";
+
+    connection.query(sql, [email], async (err, result) => {
+        if (err) {
+            return res.json({ success: false, message: "Database error" });
+        }
+
+        if (result.length === 0) {
+            return res.json({
+                success: false,
+                message: "Invalid email or password",
+            });
+        }
+
+        const user = result[0];
+
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if (!isMatch) {
+            return res.json({
+                success: false,
+                message: "Invalid email or password",
+            });
+        }
+
+        return res.json({
+            success: true,
+            user,
+        });
+    });
+});
+
+app.put("/updateStatus/:id", (req, res) => {
+    const { id } = req.params;
+    const { completed } = req.body;
+
+    const sql = "UPDATE tasks SET completed = ? WHERE id = ?";
+
+    connection.query(sql, [completed, id], (err, result) => {
+        if (err) {
+            return res.status(500).json(err);
+        }
+
+        res.json({
+            message: "Status updated successfully",
+        });
+    });
+});
+
+app.get("/reminders/:userId", (req, res) => {
+    const userId = req.params.userId;
+    const sql = `
+SELECT 
+    id,
+    task_name,
+    end_date,
+    DATEDIFF(CAST(end_date AS DATE), CURDATE()) AS days_left
+FROM TASKS
+WHERE user_id = ?
+  AND completed = FALSE
+  AND is_deleted = FALSE
+  AND DATEDIFF(CAST(end_date AS DATE), CURDATE()) BETWEEN 0 AND 5
+ORDER BY end_date ASC;
+`;
+    connection.query(sql, [userId], (err, result) => {
+        if (err) {
+            return res.status(500).json(err);
+        }
+
+        res.json(result);
+    });
+});
+
+// app.put("/updateTask/:id", (req, res) => {
+//     const { id } = req.params;
+//     const { task_name, priority } = req.body;
+
+//     const sql = `
+//         UPDATE TASKS
+//         SET task_name = ?, priority = ?
+//         WHERE id = ?
+//     `;
+
+//     connection.query(
+//         sql,
+//         [task_name, priority, id],
+//         (err, result) => {
+//             if (err) {
+//                 console.log(err);
+//                 return res.status(500).json({
+//                     message: "Error updating task"
+//                 });
+//             }
+
+//             res.json({
+//                 message: "Task updated successfully"
+//             });
+//         }
+//     );
+// });
+
+app.put("/updateTask/:id", (req, res) => {
+    const { id } = req.params;
+
+    const { task_name, priority, start_date, end_date } = req.body;
+
+    const sql = `
+        UPDATE TASKS
+        SET task_name = ?,
+            priority = ?,
+            start_date = ?,
+            end_date = ?
+        WHERE id = ?`;
+
+    console.log(task_name, priority, start_date, end_date, id);
+
+    connection.query(
+        sql,
+        [task_name, priority, start_date, end_date, id],
+        (err, result) => {
+            if (err) {
+                console.log(err);
+                return res.status(500).json(err);
+            }
+
+            res.json({
+                message: "Task updated successfully",
+            });
+        },
+    );
+});
+
+app.listen(5000, () => {
+    console.log("Server running on port 5000");
+});
